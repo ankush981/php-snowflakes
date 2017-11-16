787f127e64c6874676fc409ba602912c6301b19e
Ankush <ankush.thakur53@gmail.com>
2017-11-17 00:27:28 +0530

    Add simple factory example

---
+++ creational_patterns/01_simple_factory.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * The Simple Factory design pattern is a subset of the Factory Method pattern.
+ * It creates objects through a single factory.
+ * 
+ */
+
+class NotifierFactory
+{
+    public static function getNotifier($notifier, $to)
+    {
+        if (empty($notifier)) {
+            throw new Exception("No notifier passed.");
+        }
+
+        switch ($notifier) {
+            case 'SMS':
+                return new SMS($to);
+                break;
+            case 'Email':
+                return new Email($to, 'Junade');
+                break;
+            default:
+                throw new Exception("Notifier invalid.");
+                break;
+        }
+    }
+}
+
+/**
+ * Much ado about nothing? Perhaps, but that's all there's to the Simple
+ * Factory pattern. A single (simple) class that produces the objects you
+ * need.
+ */
