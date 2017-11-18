ca20b7aed7261154a0fec46a575f25bfd48aac3e
Ankush <ankush.thakur53@gmail.com>
2017-11-18 16:58:57 +0530

    Add Notifier abstract class

---
+++ creational_patterns/01_simple_factory/Notifier.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Common class for all types of notifiers.
+ */
+
+abstract class Notifier {
+    protected $to;
+
+    public function __construct(string $to) {
+        $this->to = $to;
+    }
+
+    abstract public function validateTo(): bool;
+    abstract public function sendNotfication(): string;
+}
