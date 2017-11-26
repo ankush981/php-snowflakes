e6990b87d81cce99f7608b48150fce964cb308fc
Ankush <ankush.thakur53@gmail.com>
2017-11-26 11:34:46 +0530

    Add SF and UK toy factories

---
+++ creational_patterns/04_abstract_factory/SFToyFactory.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'ToyFactory.php';
+require_once 'SFMazeToy.php';
+require_once 'SFPuzzleToy.php';
+
+// The factory for producing Sab Francisco toys
+class SFToyFactory implements ToyFactory {
+    private $location = 'San Francisco';
+
+    public function makeMaze() {
+        return new SFMazeToy();
+    }
+
+    public function puzzle() {
+        return new SFPuzzleToy();
+    }
+}
---
+++ creational_patterns/04_abstract_factory/UKToyFactory.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'ToyFactory.php';
+require_once 'UKMazeToy.php';
+require_once 'UKPuzzleToy.php';
+
+// The factory for producing UK toys
+class UKToyFactory implements ToyFactory {
+    private $location = 'UK';
+
+    public function makeMaze() {
+        return new UKMazeToy();
+    }
+
+    public function puzzle() {
+        return new UKPuzzleToy();
+    }
+}
